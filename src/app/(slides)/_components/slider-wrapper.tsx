f71f83d45e9c9f9eeef972796f23936942b1146a
Julián David <julianfullstackdev@gmail.com>
2024-05-25 23:44:19 -0500

    add prettier support

--- src/app/(slides)/_components/slider-wrapper.tsx
+++ src/app/(slides)/_components/slider-wrapper.tsx
@@ -1,55 +1,61 @@
-'use client'
+'use client';
 
-import React, { useEffect } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
+import React, { useEffect } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 export function SliderWrapper({ children }: Props) {
-  const [emblaRef, emblaApi] = useEmblaCarousel()
+  const [emblaRef, emblaApi] = useEmblaCarousel();
 
   const scrollPrev = () => {
-    if (emblaApi) emblaApi.scrollPrev()
-  }
+    if (emblaApi) emblaApi.scrollPrev();
+  };
 
   const scrollNext = () => {
-    if (emblaApi) emblaApi.scrollNext()
-  }
+    if (emblaApi) emblaApi.scrollNext();
+  };
 
   useEffect(() => {
     const checkKey = (e: KeyboardEvent): void => {
-      if (!emblaApi) return
+      if (!emblaApi) return;
 
       if (e.key === 'ArrowLeft') {
-        emblaApi.scrollPrev()
+        emblaApi.scrollPrev();
       } else if (e.key === 'ArrowRight') {
-        emblaApi.scrollNext()
+        emblaApi.scrollNext();
       }
-    }
+    };
 
-    window.addEventListener('keydown', checkKey)
+    window.addEventListener('keydown', checkKey);
 
     return () => {
-      window.removeEventListener('keydown', checkKey)
-    }
-  }, [emblaApi])
+      window.removeEventListener('keydown', checkKey);
+    };
+  }, [emblaApi]);
 
   return (
-    <div className="embla w-full h-screen bg-neutral-950 relative">
-      <div className="embla__viewport  h-full" ref={emblaRef}>
-        <div className="embla__container h-full">{children}</div>
+    <div className='embla relative h-screen w-full bg-neutral-950'>
+      <div className='embla__viewport  h-full' ref={emblaRef}>
+        <div className='embla__container h-full'>{children}</div>
       </div>
 
-      <div className="mt-3 flex justify-between absolute bottom-5 right-5">
-        <button className="w-20 bg-white px-2 py-1 text-sm text-black" onClick={scrollPrev}>
+      <div className='absolute bottom-5 right-5 mt-3 flex justify-between'>
+        <button
+          className='w-20 bg-white px-2 py-1 text-sm text-black'
+          onClick={scrollPrev}
+        >
           Prev
         </button>
-        <button className="w-20 bg-white px-2 py-1 text-sm text-black" onClick={scrollNext}>
+        <button
+          className='w-20 bg-white px-2 py-1 text-sm text-black'
+          onClick={scrollNext}
+        >
           Next
         </button>
       </div>
     </div>
-  )
+  );
 }
